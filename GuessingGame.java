ffa7ee9ab9750400eb03493487fcdf2c2dabff59
Patrik Hjortberg <patrik.hjortberg@gmail.com>
2021-10-26 14:37:02 +0200

    ny init

---
+++ GuessingGame.java
@@ -0,0 +1,10 @@
+public class GuessingGame{
+	public static void main (String[] args){
+
+Guesser game = new Guesser(0,1000);
+game.start();
+
+   }
+
+
+}
---
+++ run_game.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+
+FIRSTNAME="Patrik"
+LASTNAME="Hjortberg"
+DIR_NAME="${FIRSTNAME}_${LASTNAME}_labb"
+
+echo "$FIRSTNAME $LASTNAME"
+
+mkdir "$DIR_NAME" 
+cp *.java /home/patrikhjortberg/guessingGame/$DIR_NAME 
+cd /home/patrikhjortberg/guessingGame/$DIR_NAME
+pwd
+echo "Compiling..."
+javac  GuessingGame.java
+javac Guesser.java
+echo "Running game..."
+java GuessingGame
+echo "Done!"
+echo "Removing class-files"
+rm *.class
+ls 
+
+exec bash
